b7497b75f26b8bc1e13baff8f1489dad7833ac3c
kirti <kirtishinde3520@gmail.com>
2026-06-02 16:50:27 +0530

    change in backend message

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -25,7 +25,7 @@ app.use("/api/cart", cartRoute);
 
 // Test route
 app.get("/", (req, res) => {
-  res.json({ message: "Portfolio Turbo API is up and shining 🌸 🚀" });
+  res.json({ message: "Ecom Turbo API is up and shining 🌸 🚀" });
 });
 
 const PORT = 5500;
